9774732845f61c80d443539885262bf76f042a13
PicPew00 <maxitis15@hotmail.com>
2023-05-05 20:09:19 +0300

    V0.02

--- Assets/PolyMeshWorld/2D SideScroller Pixel Art Kit Free/Scripts/PlayerController.cs
+++ Assets/PolyMeshWorld/2D SideScroller Pixel Art Kit Free/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,8 +10,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float speed = 20.0f;
     public float jumpForce = 100.0f;
     private float horizontalInput;
+    private float verticalInput;
     private float forwardInput;
 
+
+    bool isOnLadder = false;
     bool isGrounded;
 
     int currentNumberOfJumpsToPerfom;
@@ -19,14 +23,14 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody= GetComponent<Rigidbody2D>();
+        rigidbody = GetComponent<Rigidbody2D>();
 
         currentNumberOfJumpsToPerfom = 0;
     }
 
     // Update is called once per frame
     void FixedUpdate()
-    {   
+    {
 
         // Read horizontal input
         horizontalInput = Input.GetAxis("Horizontal");
@@ -37,10 +41,10 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && currentNumberOfJumpsToPerfom<maxJumpsToPerfom)
+        if (Input.GetKeyDown(KeyCode.Space) && currentNumberOfJumpsToPerfom < maxJumpsToPerfom)
         {
 
-            rigidbody.velocity= Vector3.zero;
+            rigidbody.velocity = Vector3.zero;
 
             rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 
@@ -49,6 +53,42 @@ public class PlayerController : MonoBehaviour
             currentNumberOfJumpsToPerfom++;
 
         }
+
+        GoingUpOnLadder();
+    }
+    private void GoingUpOnLadder()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.UpArrow) && isOnLadder)
+        {
+
+            Debug.Log("onladderrr....");
+            rigidbody.gravityScale = 0f;
+            transform.Translate(Vector3.up * Time.deltaTime * speed * verticalInput);
+            //TRANSLATE THE PLAYER UP ( MOVING 1.0 tile at the time )
+
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            Debug.Log(" YOU ARE ON A LADDER");
+            isOnLadder = true;
+        }
+
+    }
+
+    private void ResetAllTrigger()
+    {
+        isOnLadder = false;
+        rigidbody.gravityScale = 1f;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ResetAllTrigger();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -56,7 +96,7 @@ public class PlayerController : MonoBehaviour
         if (collision.collider.gameObject.tag == "Ground")
         {
 
-            currentNumberOfJumpsToPerfom= 0;
+            currentNumberOfJumpsToPerfom = 0;
 
         }
 
